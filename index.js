e15e3fc651077ac3b427071675e23401357b7f4d
Jinesh Patel <jinesh701@gmail.com>
2017-11-20 19:26:11 -0500

    updating functions

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+//Renders platform information for each game 
+const renderPlatformInfo = function (platformResult) {
+    const gamePlatform = [];
+
+    for (let i = 0; i <= `${platformResult.length - 1}`; i++) {
+        let getPlatforms = gamePlatform.push(platformResult[i].name);
+    }
+
+    gamePlatform.forEach(function (platformItem) {
+        $('.platforms').append(`<li>${platformItem}</li>`)
+    });
+
+    console.log(gamePlatform);
+};
+
 //Renders game information for the searched game
 const renderGameInfo = function (gameInfoResult) {
     return `<div class="js-game-data row">
@@ -6,6 +21,7 @@ const renderGameInfo = function (gameInfoResult) {
                 <ul class="platforms col-6">
                 <h3 class="col-12">Original release date:</h3>${gameInfoResult.original_release_date}
                 <h3>Platforms:</h3>
+                ${renderPlatformInfo(gameInfoResult.platforms)}
                 </ul>
                 <p class="game-description col-6">${gameInfoResult.deck} <br> <br> <span class="game-details col-12"><b>For more details about the game: <a href="${gameInfoResult.site_detail_url}" target="_blank">Click Here</a></b></span></p>
             </div>
@@ -33,16 +49,6 @@ const renderHeader = (title) => `<h3 class="col-12 live-stream-header">${title}<
 
 const displayStreamData = function (gameInfo) {
     return function (streamData) {
-        const gamePlatform = [];
-
-        for (let i = 0; i <= `${gameInfo.results[0].platforms.length - 1}`; i++) {
-            let getPlatforms = gamePlatform.push(gameInfo.results[0].platforms[i].name);
-        }
-
-        gamePlatform.forEach(function (item) {
-            $('.platforms').append(`<li>${item}</li>`)
-        });
-
         let title = 'There are no streams available :(';
         let streamsSection = '';
         if (streamData._total !== 0) {
@@ -57,7 +63,7 @@ const displayStreamData = function (gameInfo) {
         `);
 
         console.log(gameInfo);
-        console.log(streamData); 
+        console.log(streamData);
     }
 }
 
